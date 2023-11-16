8aa13874b6232672312b155b88137e1243482191
RossDev1025 <rainbowsun.pop@gmail.com>
2023-11-16 14:47:19 -0500

    fix: remove pg module

--- apps/asana-service/src/app/oauth/callback/page.tsx
+++ apps/asana-service/src/app/oauth/callback/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import ErrorPage from '../../error';
 import axios from 'axios';
 
