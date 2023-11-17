f82bca1567924bdf3dad3d2ff9cdb1eebcd7e921
RossDev1025 <rainbowsun.pop@gmail.com>
2023-11-17 14:07:59 -0500

    feat: add vercel cron

--- apps/asana-service/src/app/api/users/cron/run-users-sync-jobs/route.ts
+++ apps/asana-service/src/app/api/users/cron/run-users-sync-jobs/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.log("run users sync jobs")
   return new NextResponse(null, { status: 501, statusText: 'Not Implemented' });
 }
